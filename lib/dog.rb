c1b8044e657b24b092b2536c6889885d367c04c4
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-13 03:07:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,20 +5,23 @@ class Dog
     @breed = breed
   end
 
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-
 end
+
+
+#originally had this thing which was unneccessary
+#for the test:
+#  def name=(name)
+    #@name = name
+  #end
+
+  #def name
+    #@name
+  #end
+  
+  #def breed=(breed)
+    #@breed = breed
+  #end
+
+  #def breed
+    #@breed
+  #end
